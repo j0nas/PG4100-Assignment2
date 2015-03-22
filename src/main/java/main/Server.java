c490f9b1bfbc02c640f5a739e8190eccea5b1c95
j0nas <jonas.jensen@msn.com>
2015-03-22 21:32:19 +0100

    Javadoc headers for Server.java.

--- src/main/java/main/Server.java
+++ src/main/java/main/Server.java
@@ -22,18 +22,22 @@ public class Server implements AutoCloseable {
     private ArrayList<Book> quizBooks;
 
     /**
-     * The constructor for the class.
+     * The initializing constructor for the class.
      */
     public Server() {
         Log.s("Fetching books from database, using Config.java's settings..");
         quizBooks = Db.fetchAndParseBooks();
 
-        socket = getSocketBoundToAvailablePort();
-        if (socket == null) {
+        if ((socket = getSocketBoundToAvailablePort()) == null) {
             Log.e("Socket initialization failed, cannot continue.");
         }
     }
 
+    /**
+     * The main method of the program, which controls its execution flow.
+     *
+     * @param args Not used.
+     */
     public static void main(String[] args) {
         // Log.debugLevel = Log.LOG_VERBOSE;
         try (Server server = new Server()) {
@@ -41,13 +45,18 @@ public class Server implements AutoCloseable {
         }
     }
 
+    /**
+     * Lifecycle method for the class.
+     */
     private void start() {
         ExecutorService clientThreads = Executors.newCachedThreadPool();
         Log.s("Awaiting client connections..");
+
         while (true) {
             try {
-                clients.add(new ClientModel(clients.size() + 1, socket.accept()));
-                clientThreads.execute(newClientThread());
+                ClientModel client = new ClientModel(clients.size() + 1, socket.accept());
+                clients.add(client);
+                clientThreads.execute(newClientThread(client));
             } catch (IOException e) {
                 Log.e("An error occurred whilst waiting for connections:\n" + e.getMessage());
             }
@@ -56,9 +65,15 @@ public class Server implements AutoCloseable {
         }
     }
 
-    private Runnable newClientThread() {
+    /**
+     * Creates Runnable instances that interact with the connected clients.
+     *
+     * @param client The respective client for which the instance is to be associated with.
+     * @return A Runnable instance associated with a ClientModel instance, which
+     * represents the lifecycle of interactions performed with each connected client.
+     */
+    private Runnable newClientThread(ClientModel client) {
         return () -> {
-            ClientModel client = clients.get(clients.size() - 1);
             try (DataInputStream input = new DataInputStream(client.getSocket().getInputStream());
                  DataOutputStream output = new DataOutputStream(client.getSocket().getOutputStream())
             ) {
@@ -96,12 +111,30 @@ public class Server implements AutoCloseable {
         };
     }
 
-    private void messageClient(DataOutputStream output, String message) throws IOException {
-        output.writeUTF(message);
-        output.flush();
+    /**
+     * Sends a string to a client.
+     *
+     * @param output  The output stream to utilize for the transfer.
+     * @param message The string to be sent.
+     */
+    private void messageClient(DataOutputStream output, String message) {
+        try {
+            output.writeUTF(message);
+            output.flush();
+        } catch (IOException e) {
+            Log.e("Error on sending message: " + e.getMessage());
+        }
     }
 
-    private void checkClientAnswerAndProvideFeedback(ClientModel client, DataOutputStream output, Book currentBook, String answer) throws IOException {
+    /**
+     * Checks a given string to see if it matches the name of the author of the given book.
+     *
+     * @param client      The client which the answer belongs to.
+     * @param output      The output stream for providing the respective client.
+     * @param currentBook The book which the client was queried about.
+     * @param answer      The string answer which the client submitted.
+     */
+    private void checkClientAnswerAndProvideFeedback(ClientModel client, DataOutputStream output, Book currentBook, String answer) {
         boolean scored = answer.toLowerCase().contains(currentBook.getAuthorFirstName().toLowerCase()) &&
                 answer.toLowerCase().contains(currentBook.getAuthorLastName().toLowerCase());
 
@@ -112,7 +145,13 @@ public class Server implements AutoCloseable {
         client.incCurrentQuestion(scored);
     }
 
-    private void clientFinishedQuiz(ClientModel client, DataOutputStream output) throws IOException {
+    /**
+     * Messages client final goodbyes and tells the client program to shutdown, upon receiving termination condition.
+     *
+     * @param client The client to shutdown.
+     * @param output The communication channel though which to message the shutdown.
+     */
+    private void clientFinishedQuiz(ClientModel client, DataOutputStream output) {
         Log.v(String.format("Client #%d finished quiz with score %d.",
                 client.getId(), client.getScore()));
 
@@ -121,6 +160,11 @@ public class Server implements AutoCloseable {
         messageClient(output, PREFIX_END_OF_QUIZ);
     }
 
+    /**
+     * Searches an available port, starting from the value which Config.SERVER_PORT is initially set at.
+     *
+     * @return Returns a ServerSocket object bound up to the discovered port.
+     */
     private ServerSocket getSocketBoundToAvailablePort() {
         ServerSocket serverSocket;
         while (true) {
@@ -141,6 +185,9 @@ public class Server implements AutoCloseable {
         }
     }
 
+    /**
+     * Method in accordance with the AutoClosable interface.
+     */
     @Override
     public void close() {
         try {
