b984678fac1e863430729e35654e3e832563ca3d
j0nas <jonas.jensen@msn.com>
2015-03-22 22:05:25 +0100

    no

--- src/main/java/main/Config.java
+++ src/main/java/main/Config.java
@@ -2,7 +2,7 @@ package main;
 
 public class Config {
     public static final String DB_USER = "root";
-    public static final String DB_PASS = "notSoC0y";
+    public static final String DB_PASS = "";
     public static final String DB_HOST = "localhost";
 
     public static final String DB_NAME = "pg4100innlevering2";
